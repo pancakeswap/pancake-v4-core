4c3230d241fc4290470855fc2d8b80df17332b80
Chef Snoopy <snoopy@pancakeswap.com>
2024-08-05 10:31:35 +0800

    feat: Remove PausableRole

---
+++ src/Owner.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (C) 2024 PancakeSwap
+pragma solidity ^0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+
+/// @notice Allow owner to pause in case of emergency
+abstract contract Owner is Ownable, Pausable {
+    constructor() Ownable(msg.sender) {}
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
