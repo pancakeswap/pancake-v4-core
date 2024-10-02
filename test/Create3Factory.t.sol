e01946c6bcf233aa9bb6af8bb5709dbf442099d1
ChefMist <133624774+ChefMist@users.noreply.github.com>
2024-10-02 10:39:35 +0800

    example: create3

---
+++ test/Create3Factory.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {GasSnapshot} from "forge-gas-snapshot/GasSnapshot.sol";
+import "forge-std/Test.sol";
+import {CREATE3} from "solmate/src/utils/Create3.sol";
+import {Vault} from "../src/Vault.sol";
+
+/// @notice Through the use of solmate create3, deploy contracts with deterministic addresses
+contract Create3Factory {
+    /// @notice deploy a contract with a deterministic address based on `salt + msg.sender (deployer)`
+    /// @dev this means that two contract with different creationCode can be deployed on the same address on different chains
+    function deploy(bytes32 salt, bytes memory creationCode) external payable returns (address deployed) {
+        // hash salt with the deployer address to give each deployer its own namespace
+        salt = keccak256(abi.encodePacked(msg.sender, salt));
+        return CREATE3.deploy(salt, creationCode, msg.value);
+    }
+
+    /// @notice get the deployed address of a contract with a deterministic address based on `salt + deployer`
+    function getDeployed(address deployer, bytes32 salt) public view returns (address deployed) {
+        // hash salt with the deployer address to give each deployer its own namespace
+        salt = keccak256(abi.encodePacked(deployer, salt));
+        return CREATE3.getDeployed(salt);
+    }
+
+    /// @notice execute a call on a deployed contract
+    function execute(bytes32 salt, bytes calldata data) external {
+        address target = getDeployed(msg.sender, salt);
+        target.call(data);
+    }
+}
+
+contract Create3FactoryTest is Test, GasSnapshot {
+    Create3Factory create3Factory;
+    Vault vault;
+
+    function setUp() public {
+        create3Factory = new Create3Factory();
+    }
+
+    function test_deploy_NonDeterministic() public {
+        // deploy
+        vault = new Vault();
+        snapLastCall("Create3FactoryTest#test_deploy_NonDeterministic");
+    }
+
+    function test_deploy() public {
+        // deploy
+        bytes memory creationCode = type(Vault).creationCode;
+        bytes32 salt = bytes32(uint256(0x1234));
+        address deployed = create3Factory.deploy(salt, creationCode);
+        snapLastCall("Create3FactoryTest#test_deploy");
+
+        vault = Vault(deployed);
+        assertEq(vault.owner(), address(create3Factory)); // fail as owner is the proxy contract, not factory
+    }
+}
