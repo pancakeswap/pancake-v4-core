d2f07fe5b7d15eb9d48333996eca5f4be686d3d8
chef-burger <137024020+chefburger@users.noreply.github.com>
2024-10-30 13:15:04 +0800

    Update deploy scripts (#194)
    * forge install: pancake-create3-factory
    
    * deployment: update scripts to adapt to Create3Factory
    
    * docs: update deployment scripts
    
    * chore: corrected comments for deployment scripts
    
    * chore: updated README

--- script/02_DeployCLPoolManager.s.sol
+++ script/02_DeployCLPoolManager.s.sol
@@ -5,24 +5,48 @@ import "forge-std/Script.sol";
 import {BaseScript} from "./BaseScript.sol";
 import {IVault} from "../src/interfaces/IVault.sol";
 import {CLPoolManager} from "../src/pool-cl/CLPoolManager.sol";
+import {Create3Factory} from "pancake-create3-factory/src/Create3Factory.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
+ * Step 1: Deploy
  * forge script script/02_DeployCLPoolManager.s.sol:DeployCLPoolManagerScript -vvv \
  *     --rpc-url $RPC_URL \
  *     --broadcast \
- *     --slow \
- *     --verify
+ *     --slow
+ *
+ * Step 2: Get the ABI-encoded form of the constructor arguments
+ * cast abi-encode "Constructor(address)" <vault_addr>
+ *
+ * Step 3: Verify
+ * forge verify-contract <address> CLPoolManager --watch --chain <chain_id> \
+ *     --constructor-args <constructor_args_from_step2>
  */
 contract DeployCLPoolManagerScript is BaseScript {
+    function getDeploymentSalt() public pure override returns (bytes32) {
+        return keccak256("PANCAKE-V4-CORE/CLPoolManager/1.0");
+    }
+
     function run() public {
+        Create3Factory factory = Create3Factory(getAddressFromConfig("create3Factory"));
+
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         address vault = getAddressFromConfig("vault");
         console.log("vault address: ", address(vault));
 
-        CLPoolManager clPoolManager = new CLPoolManager(IVault(address(vault)));
-        console.log("CLPoolManager contract deployed at ", address(clPoolManager));
+        /// @dev append the vault address to the creationCode
+        bytes memory creationCode = abi.encodePacked(type(CLPoolManager).creationCode, abi.encode(vault));
+
+        /// @dev prepare the payload to transfer ownership from deployer to real owner
+        bytes memory afterDeploymentExecutionPayload =
+            abi.encodeWithSelector(Ownable.transferOwnership.selector, getAddressFromConfig("owner"));
+
+        address clPoolManager = factory.deploy(
+            getDeploymentSalt(), creationCode, keccak256(creationCode), 0, afterDeploymentExecutionPayload, 0
+        );
+        console.log("CLPoolManager contract deployed at ", clPoolManager);
 
         console.log("Registering CLPoolManager");
         IVault(address(vault)).registerApp(address(clPoolManager));
