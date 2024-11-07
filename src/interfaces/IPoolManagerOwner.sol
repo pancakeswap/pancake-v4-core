f5df2d4cef1644c6bf998e82fda6d29805957e13
chefburger <burger@pancakeswap.com>
2024-11-07 16:42:22 +0800

    refactor: [hexsen-s3] update PoolManagerOwner to support 2 step poolManager ownership transfer

--- src/interfaces/IPoolManagerOwner.sol
+++ src/interfaces/IPoolManagerOwner.sol
@@ -18,6 +18,14 @@ interface IPoolManagerOwner {
 
     /// @notice transfer the ownership of pool manager to the new owner
     /// @dev used when a new PoolManagerOwner contract is created and we transfer pool manager owner to new contract
-    /// @param newOwner the address of the new owner
-    function transferPoolManagerOwnership(address newOwner) external;
+    /// @param newPoolManagerOwner the address of the new owner
+    function transferPoolManagerOwnership(address newPoolManagerOwner) external;
+
+    /// @notice accept the ownership of pool manager, only callable by the
+    /// pending pool manager owner set by latest transferPoolManagerOwnership
+    function acceptPoolManagerOwnership() external;
+
+    /// @notice get the current pending pool manager owner
+    /// @return the address of the pending pool manager owner
+    function pendingPoolManagerOwner() external view returns (address);
 }
